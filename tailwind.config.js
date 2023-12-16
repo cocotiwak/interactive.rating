ef366330548f0384b3e3dab6a5458b5138aca9a7
danoe <anggoro@gmail.com>
2023-12-16 12:58:06 +0700

    set-up container

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      'orange': 'hsl(25, 97%, 53%)',
+      'lightGrey': 'hsl(217, 12%, 63%)',
+      'mediumGrey': 'hsl(216, 12%, 54%)',
+      'darkBlue': 'hsl(213, 19%, 18%)',
+      'veryDarkBlue': 'hsl(216, 12%, 8%)',
+      'black': 'hsl(0,0%,5.49%)',
+      'white': 'hsl(0, 0%, 100%)',
+
+    },
+    screen: {
+      'sm': '375px',
+      'lg': '1440px',
+    },
+
+    extend: {
+      fontFamily: {
+       'display': ['Overpass']
+            },
+      fontSize: {
+        base: ['15px', '24px'],
+            },
+    },
   },
   variants: {
     extend: {},
