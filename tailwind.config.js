b9e72806d47f88135ddcaf665606c0a6e9c3415c
danoe <anggoro@gmail.com>
2023-12-16 11:16:49 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './**/*.html', // sesuaikan dengan file yang akan di proses
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
